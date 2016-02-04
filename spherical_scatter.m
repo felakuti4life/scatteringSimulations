219dfdb4824c1083c8b4eeb58e901c2336775237
Ethan Geller <ethan@cmn24.stanford.edu>
2016-02-03 16:53:16 -0800

    scatter function update

--- spherical_scatter.m
+++ spherical_scatter.m
@@ -6,18 +6,57 @@ function scatter_pressure = spherical_scatter( incident, k, a)
 %   a: radius of shpere
     r = (incident(1)^2 + incident(2)^2)^0.5;
     phi = atan(incident(2)/incident(1));
+    theta = acos(incident(3)/r);
+    
+    
     ka = k*a;
     n = ceil(2*(ka+1));
     M = [1:n-1].';
+    %C = M.*(M+1);
+    terms = 2*M+1
 
     J = [-besselj(1,ka); besselj(M-1,ka)-besselj(M+1,ka)];
     Y = [ bessely(1,ka); bessely(M+1,ka)-bessely(M-1,ka)];
 
-    Gamma = atan2(J,Y);
-    epsilon = [1; 2*ones(n-1,1)];
-    cosOfe = cos([0:n-1].'*phi);
-    bess = besselj([0:n-1].',k*r)+1i*bessely([0:n-1].',k*r);
-    ith = 1i.^([0:n-1].'+1);
-    scatter_pressure = -sum(epsilon.*cos(Gamma).*exp(-1i*Gamma).*cosOfe.*bess.*ith);
+    %Gamma = (-M*(k*a)^terms)/((2*M-1)^2)*(terms)*(M+1);
+    pn = zeros(n,n);
+    for m = M
+        m
+        l = legendre(m,incident(1)/r);
+        pn(m+1,:)=l(1,:);
+    end
+    
+    hank = sphh(M, k*r);
+    bess = sphj(M, k*a);
+    smooth_factor = bess./sphh(M,k*a);
+    %Gamma = atan2(J,Y);
+    %epsilon = [1; 2*ones(n-1,1)];
+    %cosOfe = cos([0:n-1].'*phi);
+    %bess = besselj([0:n-1].',k*r)+1i*bessely([0:n-1].',k*r);
+    ith = 1i.^(M.'+1);
+    scatter_pressure = -sum((terms)*ith*smooth_factor*hank*pn')
+    %scatter_pressure = -sum(epsilon.*sin(Gamma).*exp(-1i*Gamma).*cosOfe.*bess.*ith);
     
 end
+
+%spherical bessel of the first kind
+function j=sphj(n,x)
+    j=sqrt(pi/(2*x))*besselj(n+0.5,x);
+end
+
+%spherical hard bessel bessel
+function jp=sphjp(n,x)
+    jp=n/x.*sphj(n,x)-sphj(n+1,x);
+end
+
+%spherical hankel
+function h=sphh(n,x)
+    h=sqrt(pi/(2*x))*besselh(n+0.5,x);
+end
+
+%spherical hard hankel
+function hp=sphhp(n,x)
+    hp=n/x*sphh(n,x)-sphh(n+1,x);
+end
+
+
