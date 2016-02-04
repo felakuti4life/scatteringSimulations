e26de74d23ed74bc1ee5712ee601ab49aab5ffe6
Ethan Geller <ethan@cmn5.stanford.edu>
2016-02-04 12:28:52 -0800

    fixed dimensionality issue

--- spherical_scatter.m
+++ spherical_scatter.m
@@ -1,6 +1,6 @@
 function scatter_pressure = spherical_scatter( incident, k, a)
 %SPHERICAL_SCATTER scattering function for a plane wave hitting a sphere
-%   Cylinder is assumed to be located at 0.
+%   Sphere is assumed to be located at 0.
 %   Input args:
 %   incident: [x y z] vector holding cartesian coordinates of incident wave
 %   a: radius of shpere
@@ -13,30 +13,19 @@ function scatter_pressure = spherical_scatter( incident, k, a)
     n = ceil(2*(ka+1));
     M = [1:n-1].';
     %C = M.*(M+1);
-    terms = 2*M+1
+    terms = 2*M+1;
 
-    J = [-besselj(1,ka); besselj(M-1,ka)-besselj(M+1,ka)];
-    Y = [ bessely(1,ka); bessely(M+1,ka)-bessely(M-1,ka)];
-
-    %Gamma = (-M*(k*a)^terms)/((2*M-1)^2)*(terms)*(M+1);
-    pn = zeros(n,n);
-    for m = M
-        m
+    for m = M'
         l = legendre(m,incident(1)/r);
-        pn(m+1,:)=l(1,:);
+        pn(m,:)=l(1,:);
     end
     
     hank = sphh(M, k*r);
     bess = sphj(M, k*a);
     smooth_factor = bess./sphh(M,k*a);
-    %Gamma = atan2(J,Y);
-    %epsilon = [1; 2*ones(n-1,1)];
-    %cosOfe = cos([0:n-1].'*phi);
-    %bess = besselj([0:n-1].',k*r)+1i*bessely([0:n-1].',k*r);
     ith = 1i.^(M.'+1);
-    scatter_pressure = -sum((terms)*ith*smooth_factor*hank*pn')
-    %scatter_pressure = -sum(epsilon.*sin(Gamma).*exp(-1i*Gamma).*cosOfe.*bess.*ith);
-    
+
+    scatter_pressure = -sum(terms.*ith'.*smooth_factor.*hank.*pn);
 end
 
 %spherical bessel of the first kind
