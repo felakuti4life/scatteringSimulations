f2265a8cdd8f2ca500ec9acb0af2b38574b02d03
Ethan Geller <ethan.geller@gmail.com>
2016-02-22 13:59:02 -0800

    added spherical mapping functions

---
+++ animateSphereMapFromEllipsoid.m
@@ -0,0 +1,8 @@
+N = 600;
+lambda = linspace(0, 2*pi, N);
+for i = 1:N
+    mapSphereToEllipsoid(0.3, lambda(i), 0.2);
+    pause(0.03)
+    M(i) = getframe(gcf);
+end
+movie2avi(M, 'wrong_angle_draw');
---
+++ mapSphereToEllipsoid.m
@@ -0,0 +1,41 @@
+function [a,x,y] = mapSphereToEllipsoid(zeta, lambda, c)
+    N = 300;
+    draw_lambda = linspace(0, 2*pi, N);
+    draw_phi = linspace(0,2*pi, N);
+    [x,y,z] = obl2cart(zeta,draw_lambda, 0,c);
+    plot(x,z);
+    hold on
+    %lambda = 0.4;
+    [inc_x, inc_y, inc_z] = obl2cart(zeta, lambda,0, c);
+    scatter(inc_x, inc_z, 'g');
+    r = inc_z/cos(lambda);
+    xTrans = inc_x - (r*sin(lambda));
+    plotTransposedCircle(r, xTrans, 0, N);
+    axis equal;
+    hold off;
+end
+                                                              
+function [x,y] = plotTransposedCircle(r,xTrans,yTrans,N)
+    theta = linspace(0, 2*pi, N);
+    x = r * cos(theta)  + xTrans;
+    y = r * sin(theta) + yTrans;
+    plot(x,y, 'r');
+end
+
+function [ zeta,lambda, phi ] = cart2obl( x,y,z,c )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+    phi = atan(y/x);
+    zeta = real(acosh((((x^2 + y^2)^0.5) + z*1i)/c));
+    lambda = imag(acosh((((x^2 + y^2)^0.5) + z*1i)/c));
+    
+end
+
+function [ x,y,z ] = obl2cart( zeta,lambda,phi, c )
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+    x = c * cosh(zeta) * cos(lambda) .* cos(phi);
+    y = c * cosh(zeta) * cos(lambda) .* sin(phi);
+    z = c * sinh(zeta) * sin(lambda);
+
+end
